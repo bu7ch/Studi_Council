25fbb3375cdefe7bd0fe92aff0879c7cb3398d13
bu7ch <dutchr@live.fr>
2020-11-03 19:55:22 +0100

    un premier fichier pour faire des betises

---
+++ programme.js
@@ -0,0 +1,36 @@
+/*
+= assigner
+== comparaison
+=== egalité strict
+!== pas egal ou strict inégalité
+< plus petit que
+> plus grand que
+<= plus petit ou egal
+>= plus grand ou egal
+*/
+
+// let temperature = 100
+// let isFreezing = temperature <= 32
+
+// if(temperature <= 32) {
+//   console.log('il fait froid dehors !');
+// }
+// if (temperature >= 100) {
+//   console.log('Il fait vraiment trop chaud pour sortir!!');
+// }
+
+
+
+
+let age = 65
+
+if (age <= 7) {
+  console.log('vous allez avoir une reduction pour les enfants');
+} else {
+  console.log("Mais vous n'êtes pas du tout un enfant");
+}
+
+let isSenior = age >= 65
+
+// console.log(isChild);
+// console.log(isSenior);
