90b156316545b85a496e45bcee3e0499f240d1c5
bu7ch <dutchr@live.fr>
2020-12-01 20:56:34 +0100

    add DOM selector files

---
+++ DOM/selector.js
@@ -0,0 +1,10 @@
+// let tag = document.getElementById("highlight")
+// let tags = document.getElementsByClassName("bolded");
+// console.log(tags[0]);
+
+// let tags = document.getElementsByTagName("h1");
+// console.log(tags[1]);
+// let tag = document.getElementById("#highlight")
+let boldedTags = document.querySelectorAll(".bolded")
+console.log(boldedTags);
+
