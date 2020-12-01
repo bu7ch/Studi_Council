87f499ce8f1391b3535176b38c743d5ecd3195ba
bu7ch <dutchr@live.fr>
2020-12-01 20:35:38 +0100

    add file DOM

---
+++ DOM/script.js
@@ -0,0 +1,17 @@
+// let body = document.querySelector("body");
+// const isBlue = false;
+
+// setInterval(function(){
+//   if (isBlue) {
+//     body.style.background="white"
+//   } else {
+//     body.style.background = "#3498db"
+//   }
+//   isBlue = !isBlue;
+// }, 1000)
+
+document.getElementById()
+document.getElementsByClassName()
+document.getElementsByTagName()
+document.querySelector()
+document.querySelectorAll()
