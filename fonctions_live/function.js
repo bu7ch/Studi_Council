5b285007443df68e1d5371378be16c868b42a96e
bu7ch <dutchr@live.fr>
2020-12-14 16:09:18 +0100

    content of the live

---
+++ fonctions_live/function.js
@@ -0,0 +1,18 @@
+let names = ['john', 'Jane', "Mark"]
+names
+
+let years = new Array(1990,1969,1948)
+console.log(names[0]);
+
+let john = ['John', 'Smith', 1990, 'teacher',false]
+john
+
+console.log( typeof john);
+
+john.push('blue')
+john.unshift("Mr.")
+john.pop()
+john.shift()
+john
+
+console.log(john.indexOf('Smith')); 
