8f97e75849cc1585c77181177ad242bcd8cb4249
bu7ch <dutchr@live.fr>
2021-04-15 21:02:01 +0200

    toujours de code dans le jeu

--- projet-numero-1/Game.js
+++ projet-numero-1/Game.js
@@ -59,7 +59,7 @@ const Place = function (title, description){
     return existInfo
   }
   this.getTitleInfos = function() {
-    return spacer.box(this.title, this.title.length + 4, "=" )
+    return spacer.box(this.title, this.title.length + 4, "*" )
   }
 
   this.getInfo = function() {
@@ -81,16 +81,103 @@ const Place = function (title, description){
     this.exits.push(exit)
   }
 
+  this.showInfo = function() {
+    console.log(this.getInfo());
+  }
 }
 
-let bibliotheque = new Place("La vieille Bibliotheque", "Une bibliotheque avec de tres vieux livres")
+const Player = function(name, health){
+  let newLine = spacer.newline();
 
-let cuisine = new Place("Une Cuisine","une cuisine avec une odeure particulière" )
+  this.name = name
+  this.health = health
+  this.items = [];
+  this.place = null;
 
-let hall = new Place("Le Hall Principal", "Un vaste hall, etrangement vide!")
+  this.addItem = function(item){
+    this.items.push(item);
+  }
+  this.getNameInfos = function() {
+    return  this.name
+  }
+  this.getHealthInfos = function() {
+    return this.name + 'has health ' + this.health
+  }
 
-bibliotheque.addItem("une clé")
-bibliotheque.addExit(cuisine)
-bibliotheque.addExit(hall)
+  this.getPlaceInfos = function() {
+    return this.name + ' is in ' + this.place
+  }
 
-console.log(bibliotheque.showInfos());
+  this.getItemsInfos = function() {
+    let itemString = "Items: " + newLine
+    this.items.forEach(function(item){
+      itemString += " - " + item + newLine
+    })
+    return itemString
+  }
+
+  this.getInfos = function(character) {
+    let place = this.getPlaceInfos()
+    let health = this.getHealthInfos()
+    let longest = Math.max(place.length, health.length) + 4
+
+    let info = spacer.box(this.getNameInfos(), longest, character)
+
+    info += spacer.wrap(place, longest, character)
+    info += newLine + spacer.wrap(health, longest, character)
+    info += newLine + spacer.line(longest,character)
+
+    info += newLine 
+    info += " " + this.getItemsInfos()
+    info += newLine
+    info += spacer.line(longest, character)
+    info += newLine
+
+    return info
+
+  }
+
+  this.showInfo = function(character) {
+    console.log(this.getInfos(character));
+  }
+}
+
+
+// Contrôle du Jeu
+
+const render = function() {
+  console.clear()
+  player.place.showInfo()
+  player.showInfo("*")
+}
+
+const go = function(direction){
+  let place = player.place
+  let destination = place.exits[direction]
+  player.place = destination
+  render()
+  return ""
+}
+
+let rampe = new Place("L'entrée", "vous accedez a une rampe inclinée.....")
+
+let corridor = new Place("Le Coridor", " Cest un long couloir qui parcours le vaisseau")
+
+let hub = new Place("Le Centre", "Un espace spacieux ou on est aime se detendre")
+
+let pit = new Place("L'espace de maintenance", " c'est ici que nous effecuons des reparations")
+const bench = new Place("L'espace de Jeu", 'Pour jouer pres du Hub')
+
+hub.addItem('Droïd')
+corridor.addItem('Tapis')
+bench.addItem('flipper')
+pit.addItem('clé à molette')
+rampe.addExit("haut", corridor)
+corridor.addExit('continue', pit)
+
+
+const player = new Player('Bob', 50)
+player.addItem('Blaster')
+player.place = rampe
+
+render()
